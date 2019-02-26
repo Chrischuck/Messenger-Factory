96cafdc2c2cbde2f059ebcd70050d20407b35c14
Chris <chrischuck35@gmail.com>
2019-02-25 22:14:32 -0800

    Adds basic opening/closing of chat

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import './index.css'
+import Header from './components/header'
+
 export default class extends React.Component {
   render() {
 
     return (
-      <div style={{ height: '100%' }}>hello</div>
+      <div className="chat-container fadeInRight">
+        <div className="inner-chat-container">
+          <div className="chat-content">
+            <Header />
+          </div>
+        </div>
+      </div>
     )
   }
 }
