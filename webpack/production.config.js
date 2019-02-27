22b3a697fbb3e463eaabba69da741a8dceabdec1
Chris <chrischuck35@gmail.com>
2019-02-27 14:18:33 -0800

    Adds production webpack build config

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -3,65 +3,109 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const DotenvPlugin = require('webpack-dotenv-plugin');
 
-const DIST_PATH = path.resolve(__dirname, '../dist/messenger');
-const ENTRY_PATH = path.resolve(__dirname, '../src/index.js');
+const DIST_PATH = path.resolve(__dirname, '../dist/');
+const ENTRY_PATH = path.resolve(__dirname, '../');
 
-module.exports = [{
-  name: 'messenger-config',
-  mode: 'production',
-  resolve: {
-    modules: ['src', 'node_modules']
-  },
-  devtool: 'source-map',
-  entry: {
-    vendor: ['@babel/polyfill', 'react', 'react-dom'],
-    client: ENTRY_PATH,
-  },
-  output: {
-    path: DIST_PATH,
-    filename: '[name].chunkhash.bundle.js',
-    chunkFilename: '[name].chunkhash.bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(css)$/,
-        use: [
+module.exports = [
+  {
+    name: 'messenger-config',
+    mode: 'production',
+    resolve: {
+      modules: ['src', 'node_modules']
+    },
+    entry: {
+      vendor: ['@babel/polyfill', 'react', 'react-dom'],
+      client: ENTRY_PATH + '/src/index.js',
+    },
+    output: {
+      path: DIST_PATH +'/messenger',
+      filename: '[name].chunkhash.bundle.js',
+      chunkFilename: '[name].chunkhash.bundle.js',
+      publicPath: '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.(css)$/,
+          use: [
             "style-loader",
             "css-loader"
-        ]
-      },
-      { test: /\.svg$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
-     ]
+          ]
+        },
+        { test: /\.svg$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'React Chat Messenger',
+        template: './src/index.html',
+        filename: './index.html',
+        inject: true,
+        minify: {
+          collapseWhitespace: true,
+          collapseInlineTagWhitespace: true,
+          minifyCSS: true,
+          minifyURLs: true,
+          minifyJS: true,
+          removeComments: true,
+          removeRedundantAttributes: true
+        }
+      })
+    ]
   },
-  devServer: {
-    historyApiFallback: true,
-    disableHostCheck: true,
-    port: 8080
+  {
+    name: 'client-script-config',
+    mode: 'production',
+    entry: {
+      client: ENTRY_PATH + '/scripts/client.js',
+    },
+    output: {
+      path: DIST_PATH + '/scripts',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        },
+      ]
+    }
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'React Chat Messenger',
-      template: './src/index.html',
-      filename: './index.html',
-      inject: true,
-      minify: {
-        collapseWhitespace: true,
-        collapseInlineTagWhitespace: true,
-        minifyCSS: true,
-        minifyURLs: true,
-        minifyJS: true,
-        removeComments: true,
-        removeRedundantAttributes: true
-      }
-    })
-  ]
-}];
+  {
+    name: 'secondary-script-config',
+    mode: 'production',
+    entry: {
+      client: ENTRY_PATH + '/scripts/secondaryScript.js',
+    },
+    output: {
+      path: DIST_PATH + '/scripts',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        },
+      ]
+    }
+  }
+];
