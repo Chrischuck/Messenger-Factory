274be8c511f66732f9c28647329796cc6f52d33a
Chris <chrischuck35@gmail.com>
2019-02-28 18:16:42 -0800

    Adds mobile styles

--- scripts/secondaryScript.js
+++ scripts/secondaryScript.js
@@ -1,4 +1,18 @@
 !function() {
+  var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)
+  var iframe = document.createElement('iframe')
+  iframe.id = 'messenger-widget'
+  
+  if (isMobile) {
+    iframe.style = "z-index: 999999999 !important;position: fixed !important;bottom: 0;right: 0;border: 0;width: 80px;height: 80px;max-height: 100vh;max-width: 100vw;padding-top: 10px;box-sizing: border-box;"
+  } else {
+    iframe.style = "z-index: 999999999 !important;position: fixed !important;bottom: 20px;right: 20px;border: 0;width: 80px;height: 80px;max-height: 100vh;max-width: 100vw;padding-top: 10px;box-sizing: border-box;"
+  }    
+      
+  iframe.src = process.env.APP_URI
+  var injectionPoint = document.getElementsByTagName("body")[0]
+  injectionPoint.appendChild(iframe)
+
   window.addEventListener("message", receiveMessage, false);
   function receiveMessage(event) {
 
@@ -21,10 +35,3 @@
     }
   }
 }();
-
-var iframe = document.createElement('iframe')
-iframe.id = 'messenger-widget'
-iframe.style = "z-index: 999999999 !important;position: fixed !important;bottom: 20px;right: 20px;border: 0;width: 80px;height: 80px;max-height: 100vh;max-width: 100vw;padding-top: 10px;box-sizing: border-box;"
-iframe.src = process.env.APP_URI
-var injectionPoint = document.getElementsByTagName("body")[0]
-injectionPoint.appendChild(iframe)
