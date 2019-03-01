600781ad7d384ef4e80757e23ade0cf93ec70037
Chris <chrischuck35@gmail.com>
2019-02-28 18:19:27 -0800

    Updates styles for mobile

--- scripts/secondaryScript.js
+++ scripts/secondaryScript.js
@@ -8,7 +8,7 @@
   } else {
     iframe.style = "z-index: 999999999 !important;position: fixed !important;bottom: 20px;right: 20px;border: 0;width: 80px;height: 80px;max-height: 100vh;max-width: 100vw;padding-top: 10px;box-sizing: border-box;"
   }    
-      
+
   iframe.src = process.env.APP_URI
   var injectionPoint = document.getElementsByTagName("body")[0]
   injectionPoint.appendChild(iframe)
@@ -23,8 +23,13 @@
     var iframe = document.getElementById('messenger-widget')
     switch (event.data) {
       case 'OPEN':
-        iframe.style.height = '80%'
-        iframe.style.width = '400px'
+        if (isMobile) {
+          iframe.style.height = '100%'
+          iframe.style.width = '100%'
+        } else {
+          iframe.style.height = '80%'
+          iframe.style.width = '400px'
+        }
         return
       case 'CLOSE':
         iframe.style.height = '80px'
