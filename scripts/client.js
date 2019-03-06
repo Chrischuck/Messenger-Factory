7c1f3b50a2d8047b3778e5239b7fa1bd8ff4bc64
Chris <chrischuck35@gmail.com>
2019-03-06 10:36:31 -0800

    Fixes client script

--- scripts/client.js
+++ scripts/client.js
@@ -9,7 +9,7 @@ const messenger_app_id = process.env.APP_ID
   app.type = 'text/javascript'
   app.async != !0
   app.crossorigin = 'anonymous'
-  app.src = process.env.SECONDARY_SCRIPT_URI + '/' + messenger_app_id
+  app.src = process.env.SECONDARY_SCRIPT_URI + '?messenger_id=' + messenger_app_id
   var injectionPoint = document.getElementsByTagName("script")[0]
   injectionPoint.parentNode.insertBefore(app, injectionPoint)
 }();
